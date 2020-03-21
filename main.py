197daeb38c1e326aec31a452b09d2869e0ba52a0
lachie1 <lmuffett21@student.sacs.nsw.edu.au>
2020-03-21 02:36:22 +0000

    updated

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ HEIGHT = 85
 MARGIN = 1
  
 #PLAYER INSTRUCTIONS
-#1234test we123412
+#1234test we123412sfew
 
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
